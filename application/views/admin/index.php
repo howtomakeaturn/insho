c8ffab659f622c1f37a2674a9470c0630144e20a
howtomakeaturn <howtomakeaturn@hotmail.com>
2014-07-23 12:28:31 +0800

    fix link

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,7 +1,7 @@
 <a href='/admin/create'>New</a>
 <hr/>
 <?php foreach($works as $work): ?>
-    <p><a href='/demo/view/<?php echo $work->id ?>'><?php echo $work->name ?></a></p>
+    <p><a href='/<?php echo $work->url ?>' target='_blank'><?php echo $work->name ?></a></p>
     <?php button_to('', '/admin/edit', ['id'=>$work->id], 'edit') ?>
     <?php button_to('', '/admin/delete', ['id'=>$work->id], 'delete') ?>
     <hr/>

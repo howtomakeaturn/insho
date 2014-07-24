6d6c08c57e69c7d5772f487d18339dad5a71359a
howtomakeaturn <howtomakeaturn@hotmail.com>
2014-07-24 23:27:39 +0800

    add photos

--- application/views/admin/create.php
+++ application/views/admin/create.php
@@ -5,8 +5,10 @@
     <input type='text' name='ol_url' class='form-control' required />
     <p>連結名稱<i>（隨你打）</i></p>
     <input type='text' name='ol_name' class='form-control' required />
-    <p>作品</p>
+    <p>主圖</p>
     <input type="file" name='ol_image' class='form-control' required />
+    <p>系列圖</p>
+    <input type="file" name='ol_photos[]' class='form-control' multiple required />
     <hr/>
     <p><input type='submit' value='create' /></p>
 </form>
--- application/views/work/view.php
+++ application/views/work/view.php
@@ -1,1 +1,8 @@
     <img src='/upload/<?php echo $work->image ?>' />
+    <br />
+    <br />
+    <?php foreach($work->ownPhotos as $photo): ?>
+        <img src='/upload/<?php echo $photo->name ?>' />    
+        <br />
+        <br />
+    <?php endforeach; ?>

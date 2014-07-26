587f1b63e85f349ea796ae34629218be344b1977
howtomakeaturn <howtomakeaturn@hotmail.com>
2014-07-26 13:44:24 +0800

    protect index image

--- application/views/work/index.php
+++ application/views/work/index.php
@@ -3,9 +3,20 @@
     <?php foreach($works as $work): ?>
       <div class="col-sm-3 work-box">
         <a href="/<?php echo $work->url ?>" class="">
-            <img src='/upload/<?php echo $work->image ?>' />
+            <?php 
+                list($width, $height) = getimagesize('./upload/' . $work->image);
+            ?>
+            <div class='work-pic-box' style='width: <?php echo $width ?>px; height: <?php echo $height ?>px; background:url(/upload/<?php echo $work->image ?>) no-repeat; background-size: contain;'></div>
+
         </a>
       </div>
     <?php endforeach; ?>
 
 </div>
+
+<style>
+    .work-pic-box{
+        max-width: 100%;
+        max-height: 600px;
+    }
+</style>

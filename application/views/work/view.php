60d34b4b9fe326921013e27569e56ea2400728c3
howtomakeaturn <howtomakeaturn@hotmail.com>
2014-07-26 13:36:13 +0800

    protect image

--- application/views/work/view.php
+++ application/views/work/view.php
@@ -1,8 +1,21 @@
-    <img src='/upload/<?php echo $work->image ?>' />
-    <br />
-    <br />
+        <?php 
+            list($width, $height) = getimagesize('./upload/' . $work->image);
+        ?>
+        <div class='work-pic-box' style='width: <?php echo $width ?>px; height: <?php echo $height ?>px; background:url(/upload/<?php echo $work->image ?>) no-repeat; background-size: contain;'></div>
+        <br />
+        <br />
     <?php foreach($work->ownPhotos as $photo): ?>
-        <img src='/upload/<?php echo $photo->name ?>' />    
+        <?php 
+            list($width, $height) = getimagesize('./upload/' . $photo->name);
+        ?>
+        <div class='work-pic-box' style='width: <?php echo $width ?>px; height: <?php echo $height ?>px; background:url(/upload/<?php echo $photo->name ?>) no-repeat; background-size: contain;'></div>
         <br />
         <br />
     <?php endforeach; ?>
+
+<style>
+    .work-pic-box{
+        max-width: 100%;
+        max-height: 600px;
+    }
+</style>
